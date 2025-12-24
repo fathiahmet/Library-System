5fe25d617a5329405a57b1d4373a5bfd53fdb5c1
Fathi Ahmed <fathiahmet9@gmail.com>
2025-12-24 17:48:21 +0300

    Added implimentation to member class

--- MEMBER.java
+++ MEMBER.java
@@ -1,18 +1,57 @@
 public class MEMBER {
 
-  public Integer memberId;
+  private Integer memberId;
+  private String name;
+  private String email;
+  private String phoneNumber;
 
-  public String name;
-
-  public String email;
-
-  public String phoneNumber;
-
-//methods
-  public void borrowBook() {
+  // Constructor
+  public MEMBER(Integer memberId, String name, String email, String phoneNumber) {
+      this.memberId = memberId;
+      this.name = name;
+      this.email = email;
+      this.phoneNumber = phoneNumber;
   }
 
-  public void returnBook() {
+  // Getters and Setters
+  public Integer getMemberId() {
+      return memberId;
   }
 
+  public void setMemberId(Integer memberId) {
+      this.memberId = memberId;
+  }
+
+  public String getName() {
+      return name;
+  }
+
+  public void setName(String name) {
+      this.name = name;
+  }
+
+  public String getEmail() {
+      return email;
+  }
+
+  public void setEmail(String email) {
+      this.email = email;
+  }
+
+  public String getPhoneNumber() {
+      return phoneNumber;
+  }
+
+  public void setPhoneNumber(String phoneNumber) {
+      this.phoneNumber = phoneNumber;
+  }
+
+  // Methods
+  public void borrowBook(String bookTitle) {
+      System.out.println(name + " borrowed the book: " + bookTitle);
+  }
+
+  public void returnBook(String bookTitle) {
+      System.out.println(name + " returned the book: " + bookTitle);
+  }
 }
